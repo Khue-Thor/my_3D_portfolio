76e567636d66b79f3ea4c9a26327eae4e9627aaa
khuephamy <khuephumy19@gmail.com>
2023-04-07 20:41:01 -0500

    init

---
+++ REAME.md
@@ -0,0 +1,3 @@
+Install tools:
+
+- npm install --legacy-peer-deps @react-three/fiber @react-three/drei maath react-tilt react-vertical-timeline-component @emailjs/browser framer-motion react-router-dom
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+
+import {styles} from "../styles";
+import {navLinks} from "../constants";
+import {logo, menu, close} from "../assets";
+
+const Navbar = () => {
+
+  return (
+    <nav className={`${styles.paddingX} w-full flex item-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className='w-full flex justify-between items-center max-w-7x1 mx-auto'>
+
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
