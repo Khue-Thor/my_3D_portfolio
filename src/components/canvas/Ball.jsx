331e82eebc6cc1f9e43875abc7291e1f18efdb78
khuephamy <khuephumy19@gmail.com>
2023-04-08 19:55:31 -0500

    feature tech balls

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -9,7 +9,11 @@ const Ball = (props) => {
     <Float speed={1.75} rotationIntensity={1}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
-    
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronBufferGeometry args={[1, 1]} />
+        <meshStandardMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading />
+        <Decal position={[0, 0, 1]} rotation={[2 * Math.PI,0, 6.25]} map={decal} />
+      </mesh>
     </Float>
   );
 };
