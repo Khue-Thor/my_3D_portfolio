fbe3d2b7054008edbf9330ac36854ae8a2778f20
khuephamy <khuephumy19@gmail.com>
2023-04-09 14:53:21 -0500

    featured/form/earthcanvas

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+import * as random from 'maath/random/dist/maath-random.esm'
 const Stars = () => {
   return (
     <div>Stars</div>
   )
 }
 
-export default Stars
+
+const StarsCanvas = () => {
+  return (
+    <div className="w-full h-full absolute">
+      
+    </div>
+  )
+}
+
+export default StarsCanvas
